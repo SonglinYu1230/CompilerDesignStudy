060a0ec6a402976fbd765c423d65596cc55aed66
yumumu <webmanyu@gmail.com>
2020-06-07 23:45:10 +0800

    antlr test

---
+++ src/simple_parser.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+# -*- coding=utf-8 -*-
+
+from ast_node import SimpleASTNode
+from token_reader import TokenReader
+
+
+class SimpleParser:
+    def __init__(self):
+        pass
+
+
+    def parse(self, script):
+        pass
+
+
+    def prog(self, token_reader):
+        pass
+
+
+    def expression_statement(self, token_reader):
+        pass
+
+
+    def assignment_statement(self, token_reader):
+        pass
+
+
+    def int_declare(self, token_reader):
+        pass
+
+
+    def additive(self, token_reader):
+        pass
+
+
+    def multiplicative(self, token_reader):
+        pass
+
+
+    def primary(self, token_reader):
+        pass
+        
+        
+    # def dump_AST(self, node, in)
+        
+
+        
+        
---
+++ src/simple_script.py
@@ -0,0 +1,133 @@
+#!/usr/bin/env python
+# -*- coding=utf-8 -*-
+
+import sys
+from simple_parser import SimpleParser
+from ast_node import ASTNode
+from ast_node_type import ASTNodeType
+
+
+# /**
+#  * 一个简单的脚本解释器。
+#  * 所支持的语法，请参见SimpleParser.java
+#  *
+#  * 运行脚本：
+#  * 在命令行下，键入：java SimpleScript
+#  * 则进入一个REPL界面。你可以依次敲入命令。比如：
+#  * > 2+3;
+#  * > int age = 10;
+#  * > int b;
+#  * > b = 10*2;
+#  * > age = age + b;
+#  * > exit();  //退出REPL界面。
+#  *
+#  * 你还可以使用一个参数 -v，让每次执行脚本的时候，都输出AST和整个计算过程。
+#  *
+#  */
+
+class SimpleScript:
+    def __init__(self):
+        self.variables = dict()
+        self.verbose = False
+
+    
+    def start(self):
+        # print "This is the name of the script: ", sys.argv[0]
+        # print "Number of arguments: ", len(sys.argv)
+        # print "The arguments are: " , str(sys.argv)
+        if len(sys.argv) and sys.argv[1] == '-v':
+            self.verbose = True
+            print('verbose mode')
+        
+        print('Simple script language')
+
+        parser = SimpleParser()
+        script = SimpleScript()
+
+        script_text = ''
+        print('\n>') # 提示符
+
+        while True:
+            try:
+                line = input("\n>") 
+                line = line.strip()
+                if line == 'exit();':
+                    print('Good bye!')
+                    break
+                script_text += line + '\n'
+                if line.endswith(';'):
+                    tree = parser.parse(script_text)
+                    if (self.verbose):
+                        tree.dump_ast('')
+                    script.evaluate(tree, '')
+            except Exception as e:
+                print(e)
+                script_text = ''
+
+
+    def evaluate(self, node, indent):
+        """[遍历AST，计算值]
+
+        Args:
+            node ([ASTNode]): [description]
+            indent ([str]): [description]
+        """
+        result = None
+        node_type = node.node_type
+        if self.verbose:
+            print(indent + 'Calculating: ' + node_type)
+        
+        if node_type == ASTNodeType.Programm:
+            for child in node.children:
+                result = self.evaluate(child, indent)
+        elif node_type == ASTNodeType.Additive:
+            child1 = node.children[0]
+            value1 = self.evaluate(child1, indent + '\t')
+            child2 = node.children[1]
+            value2 = self.evaluate(child2, indent + '\t')
+
+            if node.text == '+':
+                result = value1 + value2
+            else:
+                result = value1 - value2
+        elif node_type == ASTNodeType.Multiplicative:
+            child1 = node.children[0]
+            value1 = self.evaluate(child1, indent + '\t')
+            child2 = node.children[1]
+            value2 = self.evaluate(child2, indent + '\t')
+
+            if node.text == '*':
+                result = value1 * value2
+            else:
+                result = value1 / value2
+        elif node_type == ASTNodeType.IntLiteral:
+            result = int(node.text)
+        elif node_type == ASTNodeType.Identifier:
+            var_name = node.text
+            if var_name in self.variables:
+                value = self.variables[var_name]
+                if value:
+                    result = int(value)
+                else:
+                    raise Exception('variable' + var_name + 'has not been set any value')
+            else:
+                raise Exception('unknown variable: ' + var_name)
+        elif node_type == ASTNodeType.AssignmentStmt:
+            var_name = node.text
+            if var_name not in self.variables:
+                raise Exception('unknown variable: ' + var_name)
+        elif node_type == ASTNodeType.IntDeclaration:
+            var_name = node.text
+            var_value = None
+
+        
+        return result
+        
+
+
+        # Python program showing  
+# a use of raw_input() 
+  
+# g = raw_input("Enter your name : ") 
+# print g 
+
